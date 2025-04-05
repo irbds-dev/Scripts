35f81382f9b7dc7db097dd38ba3eba394b495af4
igor rafael <igor.briano1998@gmail.com>
2025-04-04 22:15:50 -0300

    adicionado descrição de objetivo e descrição das variaveis

--- backup.sh
+++ backup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+#Script para criar copia de diretorios, caso o diretorio de destino esteja conectado ao GitHub
+#é possivel atualizar o repositorio passando como argumento 'git' ao chamar o script ex: 'backup.sh git'
+
+#explicando variaveis:
+#caminho = o diretorio base, aquele que contem os subdiretorios que deseja copiar
+#pasta = Os diretorios que serão copiados 
+#dest = O diretorio de destino, onde as pastas copiadas serão coladas
+
 #2025/04/04 Criado por irbds-dev 
 
 
 ## Edite conforme necessidade 
-caminho="/home/${USER}/.config"
-pasta=("/i3/" 
-	"/i3status/" 
-	"/rofi/"
+caminho="/home/${USER}/.config/"
+pasta=("i3/" 
+	"i3status/" 
+	"rofi/"
 )
 dest="${caminho}/BACKUP-CONF/"
+
+## Lista de argumento
 arg=$1
 
 
